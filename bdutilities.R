a529562ff7c4fdb41570cafb627dbdc52fd840bc
Thiloshon <thiloshon@gmail.com>
2019-05-14 18:24:24 +0530

    addning folder structure and data retrieval module

---
+++ bdutilities.R
@@ -0,0 +1,14 @@
+#' bdutilities: Reusable supporting functions across the bdverse.
+#'
+#' Internal functions required in bdverse package system.
+#'
+#'
+#'
+#' @section Citation:
+#' \itemize{
+#' \item Gueta, T., Barve, V., Nagarajah, T., & Carmel, Y. (2019). bdutilities: Reusable supporting functions across the bdverse (R package V 0.0.1). Retrieved from https://github.com/bd-R/bdutilities/
+#' }
+#'
+#' @docType package
+#' @name bdutilities
+NULL
