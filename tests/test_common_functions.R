c6a60b1dd3970ae705e73af43ab86afdb12b594d
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-03-26 18:20:03 +0530

    Created unit tests
    run `testthat::test_dir("tests")` in root of the project folder

---
+++ tests/test_common_functions.R
@@ -0,0 +1,16 @@
+source("../R/common_functions.R", chdir = TRUE)
+
+library(testthat)
+
+test_that("test NA in summarize_dataframe", {
+    expect_true(is.na(summarize_dataframe(NA)))
+})
+
+test_that("test NA in return_core", {
+    expect_true(is.na(return_core(NA)))
+})
+
+test_that("test NA in flatten_data", {
+    expect_true(is.na(flatten_data(NA)))
+})
+
