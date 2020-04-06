febf4b20fd7fe0d6ba93e7f1211682caead43424
AaradhyaSaxena <aaradhya271197@gmail.com>
2020-04-06 06:08:18 +0530

    removed_context

--- tests/testthat/test_bdutilities.R
+++ tests/testthat/test_bdutilities.R
@@ -1,7 +1,6 @@
 library(testthat)
 
 # Test summarize_dataframe function
-context("data_checks")
 test_that("summarize_dataframe", {
     # No arguments provided
     expect_error(summarize_dataframe())
@@ -14,10 +13,8 @@ l1 <- list("a", "b", c(1, 2, 3), "TRUE")
 l2 <- list("c", "d", c(4, 5, 6), "FALSE")
 l3 <- rbind(l1, l2)
 df2 <- data.frame(l3)
-s <- "bdu"
 
 # Test summarize_dataframe function
-context("data_checks")
 test_that("flatten_data", {
     # No arguments provided
     expect_error(flatten_data())
@@ -26,7 +23,6 @@ test_that("flatten_data", {
     # dataframe with list
     expect_error(expect_equal(flatten_data(df2), df2))
     # input not a dataframe
-    expect_error(expect_equal(flatten_data(s), s))
     expect_error(expect_equal(flatten_data(a), a))
     expect_error(expect_equal(flatten_data(l1), l1))
 })
