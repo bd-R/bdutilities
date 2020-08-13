bb362297df7a46677b2d6ae7640869cb7929bb04
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-08-13 12:21:56 +0530

    Update tic.R

--- tic.R
+++ tic.R
@@ -1,8 +1,7 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
 do_package_checks()
 
-if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
-  # creates pkgdown site and pushes to gh-pages branch
-  # only for the runner with the "BUILD_PKGDOWN" env var set
+if (ci_on_ghactions() && ci_is_env("BUILD_PKGDOWN", "true")) {
+  get_stage("before_deploy") %>%
+    add_step(step_install_github("ropensci/rotemplate"))
   do_pkgdown()
 }
